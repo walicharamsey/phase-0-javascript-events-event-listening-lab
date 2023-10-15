5dd6fc0eb7a45c7c2356d9ae2fdb22377a226d37
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-10-15 08:25:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const element = document.getElementById("main"); // Replace 'main' with the correct ID
+    element.addEventListener('click', function () {
+      // Your event handling code here
+    });
+  });
+  
